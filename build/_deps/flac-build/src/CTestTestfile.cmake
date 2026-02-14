6e5fb993d6dc3b547804576e44b6c7ee6733374f
pupungeggang2 <kyongkyong2892@gmail.com>
2026-02-15 00:41:51 +0900

    Fix

--- build/_deps/flac-build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kyong/workspace/cpp/card/build/_deps/flac-src/src
-# Build directory: C:/Users/kyong/workspace/cpp/card/build/_deps/flac-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libFLAC")
